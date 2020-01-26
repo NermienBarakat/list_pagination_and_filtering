3303ab372b03a5c2a3bc7f364cb5c65cfe712368
Nermien Barakat <nermienbarakat@gmail.com>
2020-01-26 02:02:21 +0000

    Search function

--- js/script.js
+++ js/script.js
@@ -4,8 +4,31 @@ List Filter and Pagination - Using Vanilla JS
 ******************************************/
 // Globel Var for List item & number of items per page. 
 const list = document.querySelectorAll('.client-item');
-const ITEM_PER_PAGE = 10;
+const ITEM_PER_PAGE = 5;
+const searchInput = document.querySelector('.client-search-input');
 
+// Search query Handler
+const searchQuery = (e) => {
+    let searchText = e.target.value.toLowerCase();
+    let searchNumber = 0;
+    const filteredList = [];
+    list.forEach(item => {
+        let listItemText = item.getElementsByTagName('h3')[0].innerText.toLowerCase();
+        if (listItemText.indexOf(searchText) > -1) {
+            item.style.display = '';
+            console.log(item);
+            searchNumber++;
+            filteredList.push(item);
+           
+        } else {
+            item.style.display = 'none';
+        }
+    });
+    init(filteredList);
+    document.querySelector('.search-number').innerText = `${searchNumber} found`;
+}
+
+searchInput.addEventListener('keyup', searchQuery);
 
 const showPage = (list, currentPage) => {
     // Iterate through list item to hide
@@ -20,7 +43,6 @@ const showPage = (list, currentPage) => {
         let indexEnd = indexStart + ITEM_PER_PAGE;
         for (indexStart; indexStart < indexEnd && indexStart < list.length; indexStart++) {
             list[indexStart].style.display = 'block';
-          
         }
     }
 }
@@ -30,25 +52,26 @@ const bgColorChanges = (activeBtn) => {
     const randomColor = Math.floor(Math.random() * 16777215).toString(16);
     document.body.style.backgroundColor = "#" + randomColor;
     activeBtn.style.backgroundColor = "#" + randomColor;
-  }
+    let title = document.getElementsByTagName('h3');
+    for (let i = 0; i < title.length; i++) {
+        title[i].style.color = "#" + randomColor;
+    }
+}
 
 //generate, append, and add functionality to the pagination buttons.
 const init = (list) => {
     // calculate total pages
     const totalPages = Math.ceil(list.length / ITEM_PER_PAGE);
-
     // Create new button for each page
     const pagination = document.getElementById('pagination');
+    pagination.innerHTML = '';
     let paginationBtn;
     for (let i = 0; i < totalPages; i++) {
-
         paginationBtn = document.createElement('BUTTON');
         paginationBtn.classList.add('js-pagination-btn');
         if (i === 0) {
             paginationBtn.classList.add('js-active-btn')
-            
         }
-
         const text = document.createTextNode(`${i + 1}`);
         paginationBtn.appendChild(text);
         pagination.appendChild(paginationBtn);
@@ -65,8 +88,6 @@ const init = (list) => {
     }
     showPage(list, 1);
 }
-
-
 
 init(list);
 
